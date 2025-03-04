560e15276ecdc04e444943e8bf32251a7692a609
Nik <nikolasgrubits@hotmail.com>
2025-03-04 23:12:18 +1100

    Testing

--- src/main/java/com/example/ancienttomes/event/ModEvents.java
+++
@@ -1,9 +0,0 @@
-package com.example.ancienttomes.event;
-
-import net.minecraftforge.fml.common.Mod;
-import com.example.ancienttomes.AncientTomes;
-
-@Mod.EventBusSubscriber(modid = AncientTomes.MODID)
-public class ModEvents {
-
-}
